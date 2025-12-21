c69b929b76b392abf0e0f696d55932e54b0f17e7
Rafael Ehlert <ra.faehlert@hotmail.com>
2025-12-21 20:27:42 -0300

    feat: add Address value object with street, number, neighborhood, complement, city, state, and zipcode properties

--- BeautyClinic.Core/ValueObjects/Address.cs
+++ BeautyClinic.Core/ValueObjects/Address.cs
@@ -1,7 +1,16 @@
 ﻿using BeautyClinic.Core.Base;
+using BeautyClinic.Core.Models.Person;
 
 namespace BeautyClinic.Core.ValueObjects;
 
 public class Address : BaseEntity
 {
+    public string Street { get; set; } = string.Empty;
+    public string Number { get; set; } = string.Empty;
+    public string Neighborhood { get; set; } = string.Empty;
+    public string Complement { get; set; } = string.Empty;
+    public string City { get; set; } = string.Empty;
+    public string State { get; set; } = string.Empty;
+    public string ZipCode { get; set; } = string.Empty;
+    public List<Individual> Individuals { get; set; } = new();
 }
