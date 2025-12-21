d1811786820a98836b0b064a3fc684dd74652b12
Rafael Ehlert <ra.faehlert@hotmail.com>
2025-12-21 20:32:03 -0300

    feat: Introduce `Procedure` entity and `EPaymentMethod` enumeration.

--- BeautyClinic.Core/Models/Procedure/Procedure.cs
+++ BeautyClinic.Core/Models/Procedure/Procedure.cs
@@ -1,7 +1,16 @@
 ﻿using BeautyClinic.Core.Base;
+using BeautyClinic.Core.Enums;
 
 namespace BeautyClinic.Core.Models.Procedure;
 
 public class Procedure : BaseEntity
 {
+    public string ProcedureName { get; set; } = string.Empty;
+    public int DurationInMinutes { get; set; }
+    public string EquipmentUsed { get; set; } = string.Empty;
+    public string ConsumedProducts { get; set; } = string.Empty;
+    public decimal Price { get; set; }
+    public EPaymentMethod PaymentMethod { get; set; }
+    public List<Appointment.Appointment> Appointments { get; set; } = new();
+    public List<ProcedurePackProcedure> ProcedurePackProcedures { get; set; } = new();
 }
